725baa514bfda9752c00cff64660a895c3cf0548
Savannah C <cynomania@gmail.com>
2019-07-04 14:57:08 -0400

    Created signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        if !logged_in?
+            erb :"/users/signup"
+        else
+            redirect :"/homepage"
+        end
+    end
+
 end
